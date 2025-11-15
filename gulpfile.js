05cb16823877fabc2c11ecbb2ca379ba8a3cb48d
Viktoria Plyuta <viktoriia.pliuta.pm.2024@lpnu.ua>
2025-11-15 18:03:22 +0200

    Обновлён gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,63 +1,80 @@
 const { src, dest, watch, series, parallel } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const cssnano = require('gulp-cssnano');
-const rename = require('gulp-rename');
-const uglify = require('gulp-uglify');
-const concat = require('gulp-concat');
-const imagemin = require('gulp-imagemin');
-const fileInclude = require('gulp-file-include');
 const browserSync = require('browser-sync').create();
+const { deleteAsync } = require('del');
+const imagemin = require('gulp-imagemin');
 
-// HTML
-function htmlTask() {
-    return src('app/html/*.html')
-    .pipe(fileInclude())
-    .pipe(dest('dist'))
-    .pipe(browserSync.stream());
-}
-
-// SCSS
-function scssTask() {
-  return src('app/scss/*.scss')
-    .pipe(sass())
-    .pipe(cssnano())
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(dest('dist/css'))
-    .pipe(browserSync.stream());
-}
-
-// JS
-function jsTask() {
-  return src('app/js/*.js')
-    .pipe(concat('script.min.js'))
-    .pipe(uglify())
-    .pipe(dest('dist/js'))
-    .pipe(browserSync.stream());
-}
-
-// IMAGES
-function imgTask() {
-  return src('app/img/*')
-    .pipe(imagemin())
-    .pipe(dest('dist/imgs'))
-    .pipe(browserSync.stream());
-}
-
-// SERVE
-function serve() {
-  browserSync.init({
-    server: {
-      baseDir: 'dist'
+const paths = {
+    html: {
+        src: 'src/index.html',
+        dest: 'dist/'
+    },
+    styles: {
+        src: 'src/scss/**/*.scss',
+        dest: 'dist/css/'
+    },
+    images: {
+        src: 'src/img/**/*',
+        dest: 'dist/img/'
+    },
+    bootstrap: {
+        css: {
+            src: 'node_modules/bootstrap/dist/css/bootstrap.min.css',
+            dest: 'dist/css/'
+        },
+        js: {
+            src: 'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
+            dest: 'dist/js/'
+        }
     }
-  });
-  watch('app/html/*.html', htmlTask);
-  watch('app/scss/*.scss', scssTask);
-  watch('app/js/*.js', jsTask);
-  watch('app/img/*', imgTask);
+};
+
+function clean() {
+    return deleteAsync(['dist']);
 }
 
-// DEFAULT TASK
-const build = parallel(htmlTask, scssTask, jsTask, imgTask);
+function html() {
+    return src(paths.html.src).pipe(dest(paths.html.dest));
+}
 
+function styles() {
+    return src(paths.styles.src)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(dest(paths.styles.dest))
+        .pipe(browserSync.stream());
+}
+
+function images() {
+    return src(paths.images.src, { encoding: false })
+        .pipe(imagemin())
+        .pipe(dest(paths.images.dest))
+        .pipe(browserSync.stream());
+}
+
+function copyBootstrapCSS() {
+    return src(paths.bootstrap.css.src).pipe(dest(paths.bootstrap.css.dest));
+}
+
+function copyBootstrapJS() {
+    return src(paths.bootstrap.js.src).pipe(dest(paths.bootstrap.js.dest));
+}
+
+function server() {
+    browserSync.init({
+        server: {
+            baseDir: './dist/'
+        }
+    });
+
+    watch(paths.html.src, html).on('change', browserSync.reload);
+    watch(paths.styles.src, styles);
+    watch(paths.images.src, images).on('change', browserSync.reload);
+}
+
+const build = series(clean, parallel(html, styles, images, copyBootstrapCSS, copyBootstrapJS));
+
+exports.clean = clean;
+exports.build = build;
+exports.serve = series(build, server);
 exports.default = build;
-exports.serve = series(build, serve);
+
